85683e73837527aef36b67d27be373293929a595
os119 <osamahalqazlan98@gmail.com>
2025-02-18 22:55:54 +0300

    Update

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -21,12 +21,3 @@ def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
 
 
-def viewbook(request, bookId):
-    # assume that we have the following books somewhere (e.g. database)
-    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
-    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
-    targetBook = None
-    if book1['id'] == bookId: targetBook = book1
-    if book2['id'] == bookId: targetBook = book2
-    context = {'book':targetBook} # book is the variable name accessible by the template
-    return render(request, 'bookmodule/show.html', context)
